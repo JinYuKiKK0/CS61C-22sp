0ec541c76a2aa0564f7f954e502f0a848d2a96f4
JinYu <90668645+JinYuKiKK0@users.noreply.github.com>
2025-02-25 13:31:50 +0800

    lab01 ex1 ing

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -7,13 +7,46 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    return 0;
+    char* p =str;
+    int count =0;
+    while(*p != '\0'){
+        if(*p == letter){
+            count = count +1;
+        }
+        p++;
+    }
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
+/*
+填充 DNA_SEQ 以记录每个核苷酸出现的次数。
+每个序列将以 NULL 终止符结尾，并且最多包含 20 个核苷酸。
+所有字母都将是大写。
+*/
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    char * p = dna_seq->sequence;
+    
+    while(*p != '\0'){
+        if(*p == 'A'){
+            dna_seq->A_count++;
+        }
+        if (*p == 'G')
+        {
+            dna_seq->G_count++;
+        }
+        if (*p == 'C')
+        {
+            dna_seq->C_count++;
+        }
+        if (*p == 'T')
+        {
+            dna_seq->T_count++;
+        }
+        p++;
+    }
     return;
 }
